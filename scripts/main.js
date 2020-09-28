5758072ba65b24d551131242b5c099b395edc5fa
Juan1899 <juanmanueldelarosaenriquez@gmail.com>
2020-09-27 19:48:08 -0500

    Banner Interaction.
    Added the JS code for the banner images interaction. Also, css and scss were modified in order to add the button and text of the promo image.

--- scripts/main.js
+++ scripts/main.js
@@ -16,4 +16,23 @@ function galleryHandle(){
 }
 
 setInterval(galleryHandle,2000);
-//
+
+const banner = document.querySelector('.banner');
+const bannerInfo = document.querySelector('.banner__info');
+let bannerIndex = 1;
+
+function bannerChangeHandle(){
+    if(bannerIndex == 0){
+        banner.style.backgroundImage = "url('../img/Header/HeaderImage1.png')";
+        bannerInfo.classList.add('banner__info--hidden');
+        bannerIndex =1;
+    }else{
+        banner.style.backgroundImage = "url('../img/Header/HeaderImage2.png')";
+        bannerInfo.classList.remove('banner__info--hidden');
+        bannerIndex =0;
+    }    
+    console.log(bannerIndex);
+}
+
+setInterval(bannerChangeHandle,2000);
+
